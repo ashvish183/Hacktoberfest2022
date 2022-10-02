5f2af5bc47455b380e9f5a19faefcbf30ac42aff
rajesh6161 <rajeshranjan2712000@gmail.com>
2022-10-02 14:55:20 +0530

    feat: added Javascript folder->sorting->good sorting algorithms

---
+++ Javascript/sorting/quickSort.js
@@ -0,0 +1,18 @@
+const quickSort = (arr) => {
+  const a = [...arr];
+  if (a.length < 2) return a;
+  const pivotIndex = Math.floor(arr.length / 2);
+  const pivot = a[pivotIndex];
+  const [lo, hi] = a.reduce(
+    (acc, val, i) => {
+      if (val < pivot || (val === pivot && i != pivotIndex)) {
+        acc[0].push(val);
+      } else if (val > pivot) {
+        acc[1].push(val);
+      }
+      return acc;
+    },
+    [[], []]
+  );
+  return [...quickSort(lo), pivot, ...quickSort(hi)];
+};
---
+++ Javascript/sorting/selectionSort.js
@@ -0,0 +1,14 @@
+// Selection Sort in JavaScript
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+  return arr;
+}
