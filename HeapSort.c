0c7df4e61245871430f3d38f1bb4aa16063b6f0b
Sourasish Mondal <sourasish.it043@bppimt.ac.in>
2022-10-09 19:23:38 +0530

    HeapSort with max heapify

--- HeapSort.c
+++ HeapSort.c
@@ -11,7 +11,6 @@ void heapify(int arr[], int n, int i) {
    int largest = i;
    int left = 2 * i + 1;
    int right = 2 * i + 2;
-  
    if (left < n && arr[left] > arr[largest])
       largest = left;
   
